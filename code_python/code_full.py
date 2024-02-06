3c9dd3c12794d2e7190ab770fea36bbbc1ebe189
GersonProjects <chancasevangelistagerson@gmail.com>
2024-02-06 18:10:05 -0500

    Diccionarios

--- code_python/code_full.py
+++ code_python/code_full.py
@@ -1,4 +1,5 @@
-#Tuplas:
+""" 
+       #TUPLAS
 #Son "invariables", su estructura es similar a una lista, pero no tiene ningún método y siempre va en paréntesis.
 #Las tuplas se mantienen constantes e invariables no se puede agregar, eliminar y modificar ninguno de sus elementos. 
 my_tupla = ("Gerson", "Chaev", 2024, 170)
@@ -13,3 +14,18 @@ print(type(my_list))
 
 print(f"Valor de la posicion 4 de la tupla: {my_tupla[3]}", f"\ntamanio de tupla es {len(my_tupla)}",end="\n")
 print(f"Hello I am {name}, my username is {username}. This year is {year}. My size is {size/100} meters.")
+
+"""
+        #DICCIONARIOS
+tupla = ("primero",2)
+myDictionary = {"name":"Gerson",14:"My birthday", "day":[14,"Enero","Ate"],tupla[1]: "Prueba de tupla"}
+print(myDictionary[14], myDictionary["day"])
+myDictionary["hobby"] = "baketball" #Agregando
+myDictionary["day"][2] = "Lima" #Modificando
+del myDictionary["name"] #Eliminando
+print(myDictionary)
+#Metodos del diccionario
+print(myDictionary.keys())
+print(myDictionary.values())
+
+
